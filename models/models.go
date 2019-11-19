c1ca250e91f1bfcbcc0bfc7a82403e5d874cc9e8
Simon Lindblad <simon.lindblad93@gmail.com>
2019-11-19 17:58:38 +0100

    Add initial model

---
+++ models/models.go
@@ -0,0 +1,11 @@
+package models
+
+type Session struct {
+	UniqueName   string
+	Title        string
+	Subtitle     string
+	Description  string
+	PresenterId  string
+	SlideDeckUrl string
+	Location     string
+}
