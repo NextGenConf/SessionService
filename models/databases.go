26751fe0a7f418ca0da1a70092bda270e1af2459
Simon Lindblad <simon.lindblad93@gmail.com>
2019-11-21 21:08:40 +0100

    Fix queries (#20)
    * Fix queries
    
    * Fix port

--- models/databases.go
+++ models/databases.go
@@ -108,7 +108,7 @@ func (mongo *MongoDbHandler) GetAllSessions() ([]Session, error) {
 
 /// Retrieve a session by the specific name
 func (mongo *MongoDbHandler) GetSession(uniqueName string) (Session, error) {
-	var filter = bson.M{"UniqueName": uniqueName}
+	var filter = bson.M{"_id": uniqueName}
 	var session Session
 	err := mongo.Collection.FindOne(context.Background(), filter).Decode(&session)
 	if err != nil {
