002ee259be31f352afb73e5928a9cddfc07623f2
Oliver Wheeler <oliver.kuruma@gmail.com>
2019-12-21 21:25:48 +0000

    let mongo _id field be auto-generated (#33)

--- models/models.go
+++ models/models.go
@@ -1,8 +1,8 @@
 package models
 
 type Session struct {
-	UniqueName   string `json:"uniqueName" bson:"_id"`
-	Title        string `json:"title" bson:"title"` 
+	UniqueName   string `json:"uniqueName" bson:"uniqueName"`
+	Title        string `json:"title" bson:"title"`
 	Subtitle     string `json:"subtitle" bson:"subtitle"`
 	Description  string `json:"description" bson:"description"`
 	PresenterId  string `json:"presenterId" bson:"presenterId"`
